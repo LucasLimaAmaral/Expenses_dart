b631326105769928d1b60227ca90ca683d88b675
Lucas Amaral <amaralx360@gmail.com>
2025-01-16 19:59:51 -0300

    style: DateFormat and import DateFormat

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -87,7 +88,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date ?? DateTime(1,1,1)),
                             style: TextStyle(
                               color: const Color.fromARGB(255, 58, 58, 58)
                             ),
