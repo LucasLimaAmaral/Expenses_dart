243122e55f8a3677fb314c28c379f1f1552e127c
Lucas Amaral <amaralx360@gmail.com>
2025-01-16 19:51:04 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis',
+      value: 310,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211,
+      date: DateTime.now(),
+    )
+  ];
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Grafico'),
+            ),
+          ),
+
+         Column(
+          children: [
+              ..._transactions.map((tr){
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          )
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ' + tr.value.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tr.title ?? 'Valor desconhecido',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tr.date.toString(),
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 58, 58, 58)
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              })
+          ],
+         )
+        ],
+      )
+    );
+  }
+}
