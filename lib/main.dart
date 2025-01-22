aacd0296369ad858743bbc116e3c5e5b30cbb17a
Lucas Amaral <amaralx360@gmail.com>
2025-01-22 19:34:07 -0300

    textFiled de titulo e valor + textButton e estilo

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
@@ -31,78 +29,98 @@ class MyHomePage extends StatelessWidget {
     )
   ];
 
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Card(
-              color: Colors.blue,
-              child: Text('Grafico'),
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Grafico'),
+              ),
             ),
-          ),
-
-         Column(
-          children: [
-              ..._transactions.map((tr){
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
+            Column(
+              children: [
+                ..._transactions.map((tr) {
+                  return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 10),
+                          decoration: BoxDecoration(
+                              border: Border.all(
                             color: Colors.black,
                             width: 2,
-                          )
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ' + tr.value.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple
+                          )),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'R\$ ' + tr.value.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple),
                           ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tr.title ?? 'Valor desconhecido',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              DateFormat('d MMM y')
+                                  .format(tr.date ?? DateTime(1, 1, 1)),
+                              style: TextStyle(
+                                  color: const Color.fromARGB(255, 58, 58, 58)),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ],
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Titulo',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Valor (R\$)',
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text(
+                        'Nova transação',
+                        style: TextStyle(
+                          color: Colors.purple
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title ?? 'Valor desconhecido',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(tr.date ?? DateTime(1,1,1)),
-                            style: TextStyle(
-                              color: const Color.fromARGB(255, 58, 58, 58)
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              })
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
-         )
-        ],
-      )
-    );
+        ));
   }
 }
