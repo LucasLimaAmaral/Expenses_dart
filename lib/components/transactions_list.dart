7fa4e5bb8ce31f05d50913628f76adde4e96475c
Lucas Amaral <amaralx360@gmail.com>
2025-01-22 21:05:02 -0300

    separacao em widgets

---
+++ lib/components/transactions_list.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionsList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  const TransactionsList(this.transactions, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions.map((tr) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.black,
+                    width: 2,
+                  )),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${tr.value.toString()}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tr.title ?? 'Valor desconhecido',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat('d MMM y')
+                          .format(tr.date ?? DateTime(1, 1, 1)),
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 58, 58, 58)),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        }).toList(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 
 main() => runApp(ExpensesApp());
 
@@ -14,21 +14,8 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tenis',
-      value: 310,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211,
-      date: DateTime.now(),
-    )
-  ];
-
+  
+  const MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,82 +31,7 @@ class MyHomePage extends StatelessWidget {
                 child: Text('Grafico'),
               ),
             ),
-            Column(
-              children: [
-                ..._transactions.map((tr) {
-                  return Card(
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 10),
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                            color: Colors.black,
-                            width: 2,
-                          )),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            'R\$ ' + tr.value.toString(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tr.title ?? 'Valor desconhecido',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              DateFormat('d MMM y')
-                                  .format(tr.date ?? DateTime(1, 1, 1)),
-                              style: TextStyle(
-                                  color: const Color.fromARGB(255, 58, 58, 58)),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                }).toList(),
-              ],
-            ),
-            Card(
-              elevation: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Titulo',
-                      ),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Valor (R\$)',
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () => {},
-                      child: Text(
-                        'Nova transação',
-                        style: TextStyle(
-                          color: Colors.purple
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
+            TransactionUser()
           ],
         ));
   }
